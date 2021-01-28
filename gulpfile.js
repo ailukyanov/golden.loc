9a3c1948f9b77da418bb5d0c1b74f769708393a8
Alexander Lukyanov <54592927+ailukyanov@users.noreply.github.com>
2021-01-28 07:30:36 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,16 @@
 'use strict';
 
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
-const sourcemaps = require('gulp-sourcemaps');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var sourcemaps = require('gulp-sourcemaps');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 
 sass.compiler = require('node-sass');
 
-// Общий конфиг сборщика
-const config = {
-    proxy: {
-        host: 'golden.loc',
-        port: 4000
-    },
-    watch: {
-        path: {
-            html: "**/*.html",
-            scss: "./scss/**/*.scss"
-        }
-    },
-    sass: {
-        src: './scss/**/*.scss',
-        dest: './css',
-    }
-};
-
 gulp.task('sass', function () {
-    return gulp.src(config.sass.src)
+    return gulp.src('./scss/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss([
@@ -37,18 +19,18 @@ gulp.task('sass', function () {
             })
         ]))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(config.sass.dest))
+        .pipe(gulp.dest('./css'))
         .pipe(browserSync.stream());
 
 });
 
-// Starts a BrowerSync instance
+// Starts a BrowserSync instance
 gulp.task('default', gulp.series('sass', function(){
     browserSync.init({
-        proxy: config.proxy.host,
-        port: config.proxy.port
+        server: ".",
+        port: 4000
     });
 
-    gulp.watch(config.watch.path.html).on("change", browserSync.reload);
-    gulp.watch(config.watch.path.scss, gulp.series('sass'));
+    gulp.watch("*.html").on("change", browserSync.reload);
+    gulp.watch('./scss/**/*.scss', gulp.series('sass'));
 }));
